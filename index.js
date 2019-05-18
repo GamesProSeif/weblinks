421303a1557892c7d163cdd8618d7b770ac72e4f
GamesProSeif <seifmansour2014@outlook.com>
2019-05-18 03:19:35 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// EJS Middleware
+app.set('view engine', 'ejs');
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.status(200).render('index', {title: 'Home'});
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {title: 'About'});
+});
+
+app.use('/link', require(path.join(__dirname, 'routers', 'link')));
+
+app.get('*', (req, res) => {
+  res.status(404).render('404', {title: '404 Error', url: req.url});
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
---
+++ routers/link.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+const { JSDOM } = require('jsdom');
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/', (req, res, next) => {
+  (async () => {
+    try {
+      const fetched = await fetch(req.body.url);
+      const page = await fetched.text();
+      const dom = new JSDOM(page);
+      const document = dom.window.document;
+
+      let selected = document.querySelectorAll('a');
+      let links = [];
+      for (let i = 0; i < selected.length; i++) {
+        if (selected[i].textContent && !selected[i].href.startsWith('about')) {
+          links.push({
+            name: selected[i].textContent,
+            href: selected[i].href
+          });
+        }
+      }
+
+      res.status(200).render('link', {title: 'Links', links});
+      next();
+    } catch (e) {
+      console.log(e);
+      res.status(404).send('An error occured');
+    }
+  })();
+});
+
+module.exports = router;
